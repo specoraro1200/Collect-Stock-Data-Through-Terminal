d7b9414a6e2e2bfd866aa1872f21e1ce1e9fdb10
specoraro1200 <69777511+specoraro1200@users.noreply.github.com>
2022-06-14 13:57:02 -0500

    Add files via upload

--- nas.sh
+++ nas.sh
@@ -12,14 +12,15 @@ while read p; do
 	#real=`echo $website | sed -e 's/.*"ToHundredth" streamFeed="BatsUS">\(.*\)<\/span><div class="wsod_quoteLabel">Delayed Data<div class="wsod_quoteLabelAsOf".*/\1/'`
 	#echo $real
 	annalyst=`echo $website | sed -e 's/.*The \(.*\) analysts offering.*/\1/'`
-	median=`echo $website | sed -e 's/.*median target of \(.*\), with a high.*/\1/'`
-	high=`echo $website | sed -e 's/.*high estimate of \(.*\) and a low.*/\1/'`
-	low=`echo $website | sed -e 's/.*low estimate of \(.*\). The median.*/\1/'`
-	percentage=`echo $website | sed -E 's/.*Data">(.*)%<\/span> .*/\1/'`
-
-        lastPrice=`echo $website |  sed -e 's/.*from the last price of \(.*\).<\/p><div class="wsod_chart">.*/\1/'`
+	median=`echo $website | sed -e 's/.*median target of \(.*\), with a high.*/\1/'| sed 's/,//g'` 
+	high=`echo $website | sed -e 's/.*high estimate of \(.*\) and a low.*/\1/'| sed 's/,//g'`
+	low=`echo $website | sed -e 's/.*low estimate of \(.*\). The median.*/\1/'| sed 's/,//g'`
+	percentage=`echo $website | sed -E 's/.*Data">(.*)%<\/span> .*/\1/'| sed 's/,//g'`
+	
+        lastPrice=`echo $website |  sed -e 's/.*from the last price of \(.*\).<\/p><div class="wsod_chart">.*/\1/'| sed 's/,//g'`
 	echo $p "median" $median "here" $low $high $percentage $lastPrice $annalyst
 	psql -t -U "sal" -A -d 'project' -c "insert into data (ticker,high,low,median,percentage,lastprice,annalyst,date) values ('$p',$high,$low,$median,'$percentage',$lastPrice,$annalyst,CURRENT_DATE);"
+	
 #mysql -uroot -p'Password' <<EOF
   #Use Prediction;
   #Insert into NasdaqPredictions Values ('$p',$high,$low,$median,$percentage,$lastPrice,$annalyst);
